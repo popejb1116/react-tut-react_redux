007ce3b07ead869f9868860d546885106e33e3e5
popejb1116 <popejb1116@gmail.com>
2018-12-20 15:10:05 -0500

    Completed 14 - Displaying array of items in state passed as props to separate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Ninjas from './Ninjas'
 
 class App extends Component {
+
+  state = {
+   ninjas: [
+     {name: 'Helio', age: '95', belt: 'coral', id: '1'},
+     {name: 'Royce', age: '53', belt: 'black', id: '2'},
+     {name: 'Jonathan', age: '33', belt: 'white', id: '3'},
+   ]
+  }
 
   render() {
     return (
       <div className="App">
        <h1>My first React app</h1>
-       <p>Welcome</p>
+       <Ninjas ninjas={this.state.ninjas}/>
       </div>
     )
   }
