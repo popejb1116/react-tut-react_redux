77b26ac26e3bf03180c367b99fe9c2f503a4c518
popejb1116 <popejb1116@gmail.com>
2018-12-20 16:03:52 -0500

    Completed 16 - conditional rendering

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Ninjas = ({ninjas}) =>  {
   const ninjaList = ninjas.map(ninja => {
-    return (
+
+    return ninja.age > 20 ?      
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
         <div>Age: {ninja.age}</div>
         <div>Belt: {ninja.belt}</div>
       </div>
-    )
+      : 
+      null
+      
   })
   
   return (      
