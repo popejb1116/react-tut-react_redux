ffa8146ab8e3864ae17bf1b2c28e68baca7a6dc7
popejb1116 <popejb1116@gmail.com>
2018-12-24 15:00:35 -0500

    Completed 31 - Getting and displaying individual post with route parameters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component {
   
@@ -12,9 +13,10 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About}/>
-          <Route path='/contact' component={Contact}/>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
         </div>
       </BrowserRouter>      
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
 
@@ -14,7 +15,6 @@ class Home extends Component {
       url: 'https://jsonplaceholder.typicode.com/posts'
     })
     .then(res => {
-      //console.log(res)
       this.setState({
         posts: res.data.slice(0,10)
       })
@@ -31,7 +31,9 @@ class Home extends Component {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={'/' + post.id}>
+                <span className="card-title">{post.title}</span>
+              </Link>              
               <p>{post.body}</p>
             </div>
           </div>
