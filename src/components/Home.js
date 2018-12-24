7f20267765a0a017cd95bd5ed75c32ca30c2b454
popejb1116 <popejb1116@gmail.com>
2018-12-24 12:57:58 -0500

    Completed 29 - Axios and lifecycle hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cumque commodi vel dicta adipisci, doloribus sunt reiciendis iusto exercitationem, dignissimos quod aliquam ducimus eaque amet cupiditate. Sunt fugiat non nemo!</p>            
-    </div>
-  )
+class Home extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+
+    axios({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/posts'
+    })
+    .then(res => {
+      //console.log(res)
+      this.setState({
+        posts: res.data.slice(0,10)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  render(){
+    const { posts } = this.state
+    const postsList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">
+        No Posts Yet
+      </div>
+    )
+
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postsList}      
+      </div>
+    )
+  }
 }
 
 export default Home
