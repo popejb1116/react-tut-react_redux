c9c401ef2d2f6406376c403141cbbe76a80af699
popejb1116 <popejb1116@gmail.com>
2018-12-30 15:26:48 -0500

    Completed 42 - Implemented Redux for displaying posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,15 @@
 import React, {Component} from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
-class Home extends Component {
-
-  state = {
-    posts: []
-  }
-
-  componentDidMount(){
-
-    axios({
-      method: 'get',
-      url: 'https://jsonplaceholder.typicode.com/posts'
-    })
-    .then(res => {
-      this.setState({
-        posts: res.data.slice(0,10)
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
+class Home extends Component {  
+  
   render(){
-    const { posts } = this.state
+
+    console.log(this.props)
+
+    const { posts } = this.props
     const postsList = posts.length ? (
       posts.map(post => {
         return (
@@ -58,4 +41,10 @@ class Home extends Component {
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,15 @@
+const initState = {
+  posts: [
+    {id: '1', title: 'Squirtel Laid an Egg', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illo libero voluptatem porro sequi voluptatum tempora est nobis iste ipsam! Repellat laboriosam nemo necessitatibus eligendi! Dicta asperiores porro maiores fuga?'},
+    {id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illo libero voluptatem porro sequi voluptatum tempora est nobis iste ipsam! Repellat laboriosam nemo necessitatibus eligendi! Dicta asperiores porro maiores fuga?'},
+    {id: '3', title: 'A Helix Fossil was Found', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illo libero voluptatem porro sequi voluptatum tempora est nobis iste ipsam! Repellat laboriosam nemo necessitatibus eligendi! Dicta asperiores porro maiores fuga?'}
+  ]
+}
+
+const rootReducer = (state = initState, action) => {
+  return (
+    state
+  )
+}
+
+export default rootReducer
