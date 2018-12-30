3717c6458c006a25a72ef13e47e3993ab959864c
popejb1116 <popejb1116@gmail.com>
2018-12-30 15:26:48 -0500

    Completed 41 - Implemented Redux for displaying posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import { connect } from 'react-redux'
 
-class Post extends Component {
-
-  state = {
-    post: null
-  }
-
-  componentDidMount() {
-    let id = this.props.match.params.post_id
-    axios({
-      method: 'get',
-      url: 'https://jsonplaceholder.typicode.com/posts/' + id
-    })
-    .then(res => {
-      this.setState({
-        post: res.data
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
+class Post extends Component {  
 
   render() {
     
-    let post = this.state.post ? (
+    let post = this.props.post ? (
       <div className="post left">
-        <h4>{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4>{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -42,4 +22,11 @@ class Post extends Component {
   }
 }
 
-export default Post
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id
+  return {
+    post: state.posts.find(post => post.id === id)
+  }
+}
+
+export default connect(mapStateToProps)(Post)
